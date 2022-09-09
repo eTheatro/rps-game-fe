f200540ff2e8fec1bfc5ed14cdcad81424dc59ff
Amine ZAMANI <azamani@portnet.local>
2022-09-09 18:08:33 +0100

    ci/cd impl

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React (New version 4)
+          Learn React (New version 555)
         </a>
       </header>
     </div>
